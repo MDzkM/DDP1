750c175bf70fdc83513962b76f4094e0382eecfc
Dzikra Muzaki <mdzkm17@gmail.com>
2018-12-05 17:23:33 +0700

    Tugas baru.

---
+++ A_RR_Muhammad Dzikra Muzaki_1806141334_Tugas04.py
@@ -0,0 +1,36 @@
+from tkinter import *
+# TODO: Import something to create zip
+
+class Main_GUI:
+
+    def __init__(self):
+        self.root = Tk()
+
+    def create_GUI_layout(self):
+        pass
+
+    def choose_file():
+        '''Choose file as input using GUI.'''
+
+        # Ask for input file
+        input_window = Tk()
+        input_window.withdraw()
+
+        # Return chosen file
+        return filedialog.askopenfilename()
+
+    def mainloop(self):
+        pass
+
+def main():
+    try:
+        pass
+
+    except Exception:
+        pass
+
+    finally:
+        pass
+
+if __name__ == '__main__':
+    main()
